1822fb17d033f2a5315ababf887814133c00d78b
amitstein9 <amitstein@mail.tau.ac.il>
2025-03-31 10:51:57 +0300

    After running on all dataset

--- src/datasetWrapper.py
+++ src/datasetWrapper.py
@@ -27,7 +27,7 @@ class GSM8kWrapper(AbstractDatasetWrapper):
     def __init__(self, text2image: AbstractText2Image=FixedSizeText2Image()):
         self.dataset_id = "GSM8k"
         self._text2image = text2image
-        self.dataset = load_dataset("gsm8k", "main")["test"].select(range(10))
+        self.dataset = load_dataset("gsm8k", "main")["test"]
         self.dataset = self.dataset.map(self._map_sample)
     
     #separete question, answer calculations and finel numerical answer
